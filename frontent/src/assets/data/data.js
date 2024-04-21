d0596cd01b37a2d3391db567e63b0c44014abe3f
lanararoth <165750255+lanararoth@users.noreply.github.com>
2024-04-21 19:11:23 +0530

    Add files via upload

---
+++ frontent/src/assets/data/data.js
@@ -0,0 +1,162 @@
+export const nav = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "pages",
+    url: "/pages",
+  },
+  {
+    id: 4,
+    text: "blog",
+    url: "/blog",
+  },
+  {
+    id: 5,
+    text: "contact",
+    url: "/contact",
+  },
+]
+export const category = [
+  {
+    id: 1,
+    category: "BIRDS",
+    title: "FLYYYY",
+    cover: "/birds.jpg",
+  },
+  {
+    id: 2,
+    category: "MAMMALS",
+    title: "Becoming a Dragonfly",
+    cover: "/mammals.jpg",
+  },
+  {
+    id: 3,
+    category: "INVERTIBRATES",
+    title: "There's always light at the end of the tunnel",
+    cover: "/insects.jpg",
+  },
+  {
+    id: 4,
+    category: "REPTILES",
+    title: "Stay Calm And Surf",
+    cover: "/reptiles.jpg",
+  },
+  {
+    id: 5,
+    category: "FISH",
+    title: "There's always light at the end of the tunnel",
+    cover: "/fishes.jpg",
+  },
+  {
+    id: 6,
+    category: "AMPHIBIANS",
+    title: "Becoming a Dragonfly",
+    cover: "/amphibians.jpg",
+  },
+  {
+    id: 7,
+    category: "WILDLIFE",
+    title: "Stay Calm And Surf",
+    cover: "/wildlife.jpg",
+  },
+  {
+    id: 8,
+    category: "MARINELIFE",
+    title: "There's always light at the end of the tunnel",
+    cover: "/marinelife.jpg",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    title: "All the Birds in the Sky",
+    desc: "Birds are warm-blooded vertebrates who belong to the class of Aves. They have wings and lay hard-shelled eggs i.e. they are oviparous. They have toothless beaked jaws and a high rate of metabolism. Birds have a heart that is 4-chambered and a light skeleton. There are varieties of birds that are found around the world. They measure from about 5.5 cm (bee hummingbird) to about 2.8 meters like an ostrich. There are many forms of birds and they are either perching or passerine types of birds mostly. Birds have feathers that vary according to the species. Elephant birds and moa birds which are now extinct were the birds that had no wings. Many birds have adapted themselves for swimming like those of water and seabirds. Birds have a common feature that resembles the dinosaurs with wings or theropods. They show resemblance to the ancient avialans like the archaeopteryx which were found around 160 million years ago.  The modern birds developed in the middle to late Cretaceous period according to scientific evidence.",
+    category: "BIRDS",
+    cover: "/fly.jpg",
+    date: "APRIL 05, 2024",
+  },
+
+  {
+    id: 2,
+    title: "Closest to Humans",
+    desc:  "A mammal is an animal that breathes air, has a backbone, and grows hair at some point during its life. In addition, all female mammals have glands that can produce milk. Mammals are among the most intelligent of all living creatures.Mammals include a wide variety of animals, from cats to humans to whales. There are more than 5,000 species, or kinds, of living mammal. More than half of all mammals are rodents, a group that includes mice and squirrels.",
+    category: "MAMMALS",
+    cover: "/mam.jpg",
+    date: "APRIL 15, 2024",
+  },
+  {
+    id: 3,
+    title: "Mysterious Creatures",
+    desc: "Reptiles are air-breathing, cold-blooded vertebrates that have scaly bodies rather than hair or feathers; most reptile species are egg-laying, though certain “squamates” — lizards, snakes and worm-lizards — give birth to live young. The earliest reptile is usually said to have been Hylonomus (a so-called forest mouse), which lived about 315 million years ago and resembled contemporary lizards.Reptileis an ambiguous category: It usually refers to lizards, snakes, turtles, alligators and crocodiles, but to be genetically consistent should also include birds, since crocodilians are more closely related to birds than to lizards, snakes or turtles. Turtles are so genetically distinct that many scientists recommend treating them as their own class (Chelonia) on an equal footing with birds, mammals, amphibians, fish and reptiles. Some scientists would also elevate crocodilians to the class level.",
+    category: "REPTILES",
+    cover: "/rep.jpg",
+    date: "MARCH 15, 2024",
+  },
+  {
+    id: 4,
+    title: "Animal Without Backbone",
+    desc: "An invertebrate is an animal without a backbone. In fact, invertebrates don't have any any bones at all! Invertebrates that you may be familiar with include spiders, worms, snails, lobsters, crabs and insects like butterflies. However, humans and other animals with backbones are vertebrates.",
+    category: "INVERTIBRATES",
+    cover: "/inv.jpg",
+    date: "APRIL 20, 2024",
+  },
+  {
+    id: 5,
+    title: "Nemo Tales",
+    desc: "Fish, any of approximately 34,000 species of vertebrate animals (phylum Chordata) found in the fresh and salt waters of the world. Living species range from the primitive jawless lampreys and hagfishes through the cartilaginous sharks, skates, and rays to the abundant and diverse bony fishes. Most fish species are cold-blooded; however, one species, the opah (Lampris guttatus), is warm-blooded.",
+    category: "FISH",
+    cover: "/fi.jpg",
+    date: "APRIL 20, 2024",
+  },
+  {
+    id: 6,
+    title: "World of Wonders",
+    desc: "Wildlife refers to undomesticated animal species, but has come to include all organisms that grow or live wild in an area without being introduced by humans. Wildlife was also synonymous to game: those birds and mammals that were hunted for sport. Wildlife can be found in all ecosystems.",
+    category: "WILDLIFE",
+    cover: "/wild.jpg",
+    date: "APRIL 20, 2024",
+  },
+  {
+    id: 7,
+    title: "Small Vertibrates",
+    desc: "Amphibians are ectothermic, anamniotic, four-limbed vertebrate animals that constitute the class Amphibia. In its broadest sense, it is a paraphyletic group encompassing all tetrapods, excluding the amniotes (tetrapods with an amniotic membrane, such as modern reptiles, birds, and mammals). All extant (living) amphibians belong to the monophyletic subclass Lissamphibia, with three living orders: Anura (frogs), Urodela (salamanders), and Gymnophiona (caecilians). Evolved to be mostly semiaquatic, amphibians have adapted to inhabit a wide variety of habitats, with most species living in freshwater, wetland or terrestrial ecosystems (such as riparian woodland, fossorial and even arboreal habitats). Their life cycle typically starts out as aquatic larvae with gills known as tadpoles, but some species have developed behavioural adaptations to bypass this.",
+    category: "AMPHIBIANS",
+    cover: "/amp.jpg",
+    date: "APRIL 20, 2024",
+  },
+  {
+    id: 8,
+    title: "Into The Ocean",
+    desc: "Marine life, sea life, or ocean life is the plants, animals, and other organisms that live in the salt water of seas or oceans, or the brackish water of coastal estuaries. At a fundamental level, marine life affects the nature of the planet. Marine organisms, mostly microorganisms, produce oxygen and sequester carbon. Marine life, in part, shape and protect shorelines, and some marine organisms even help create new land (e.g. coral building reefs).",
+    category: "MARINELIFE",
+    cover: "/mar.jpg",
+    date:  "APRIL 20, 2024",
+  },
+  {
+    id: 9,
+    title: "Small Vertibrates",
+    desc: "Amphibians are ectothermic, anamniotic, four-limbed vertebrate animals that constitute the class Amphibia. In its broadest sense, it is a paraphyletic group encompassing all tetrapods, excluding the amniotes (tetrapods with an amniotic membrane, such as modern reptiles, birds, and mammals). All extant (living) amphibians belong to the monophyletic subclass Lissamphibia, with three living orders: Anura (frogs), Urodela (salamanders), and Gymnophiona (caecilians). Evolved to be mostly semiaquatic, amphibians have adapted to inhabit a wide variety of habitats, with most species living in freshwater, wetland or terrestrial ecosystems (such as riparian woodland, fossorial and even arboreal habitats). Their life cycle typically starts out as aquatic larvae with gills known as tadpoles, but some species have developed behavioural adaptations to bypass this.",
+    category: "AMPHIBIANS",
+    cover: "/amp2.jpg",
+    date: "APRIL 20, 2024", 
+  },
+  {
+  id: 10,
+  title: "Mysterious Creatures",
+  desc: "Reptiles are air-breathing, cold-blooded vertebrates that have scaly bodies rather than hair or feathers; most reptile species are egg-laying, though certain “squamates” — lizards, snakes and worm-lizards — give birth to live young. The earliest reptile is usually said to have been Hylonomus (a so-called forest mouse), which lived about 315 million years ago and resembled contemporary lizards.Reptileis an ambiguous category: It usually refers to lizards, snakes, turtles, alligators and crocodiles, but to be genetically consistent should also include birds, since crocodilians are more closely related to birds than to lizards, snakes or turtles. Turtles are so genetically distinct that many scientists recommend treating them as their own class (Chelonia) on an equal footing with birds, mammals, amphibians, fish and reptiles. Some scientists would also elevate crocodilians to the class level.",
+  category: "REPTILES",
+  cover: "/rep2.jpg",
+  date: "MARCH 15, 2024"
+  }
+    
+  
+]
