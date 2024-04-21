0f5cbc464158515131e422706cd5249f62c105b5
lanararoth <165750255+lanararoth@users.noreply.github.com>
2024-04-21 19:24:14 +0530

    Add files via upload

---
+++ frontent/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import { Category } from "../../components/category/category"
+import { Card } from "../../components/blog/card"
+
+ const Home = () => {
+  return (
+    <>
+      {/*  <Slider />*/}
+      <Category />
+      <Card />
+    </>
+  )
+}
+export default Home;
